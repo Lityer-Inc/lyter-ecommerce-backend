16c2b2297210fcf4f5dbb9d4ac384c0f531979f3
Abdullahk70 <scientificgenius701924@gmail.com>
2023-12-22 18:15:44 +0500

    made the model for products using mongoose

---
+++ models/Products.js
@@ -0,0 +1,45 @@
+const { default: mongoose } = require("mongoose");
+
+const productSchema=new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+      },
+      countInStock: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: Buffer, // storing images as Buffer data
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+        default: 0.00,
+      },
+      rating: {
+        type: Number,
+        required: true,
+        default: 10,
+      },
+      isFeatured: {
+        type: Boolean,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      barcode: {
+        type: String,
+        required: true,
+      },
+      status: {
+        type: String,
+        required: true,
+      },
+});
+
+
+export const product=mongoose.Model("product",productSchema)
