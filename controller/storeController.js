8e58098d8fb451a52eb7af79da47edcc415b3cf8
Abdullahk70 <scientificgenius701924@gmail.com>
2023-12-25 18:36:56 +0500

    added get specific store endpoint

--- controller/storeController.js
+++ controller/storeController.js
@@ -16,6 +16,8 @@ export const AddStore = async (req, res, next) => {
 
   // res.status(200).json({ status: true, message: "Product added created" });
 };
+
+//gets the list of all the available stores in the mongodb
 export const getStores = async (req, res) => {
   try {
     const stores = await storeModel.find();
@@ -30,4 +32,21 @@ export const getStores = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+
+export const getSpecificStore = async (req, res) => {//returns the specific store according to the custom id provided. Not the MongoDb id
+  try {
+    const stores = await storeModel.find();
+
+    if (stores.length === 0) {
+      return res.status(404).json({ message: "No stores found" });
+    }
+
+    res.json(stores);
+  } catch (error) {
+    console.error("Error fetching stores:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 
--- routes/store.js
+++ routes/store.js
@@ -1,4 +1,4 @@
-import { getStores } from "../controller/storeController.js";
+import { getStores,getSpecificStore } from "../controller/storeController.js";
 import express from "express"
 const storeRouter = express.Router();
 import {AddStore} from '../controller/storeController.js';
@@ -6,5 +6,6 @@ import { requireRetailerAuth } from '../middleWare/authMiddleware.js';
 
 
 storeRouter.post("/add_store", requireRetailerAuth, AddStore);
-storeRouter.get("/",getStores);
+storeRouter.get("/",getStores);//returns all the stores in the mongoDb 
+storeRouter.get("/:id",getSpecificStore);//Returns the specific store with the store id
 export default storeRouter;
