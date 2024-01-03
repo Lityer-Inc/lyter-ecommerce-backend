4fd880d4897b04d667410b1bdb6a4894ac610767
mohammadashrafulislam <ixdell41@gmail.com>
2024-01-04 02:39:18 +0600

    authorization done, image upload done

--- routes/store.js
+++ routes/store.js
@@ -7,15 +7,15 @@ import { upload } from "../middleWare/multer.js";
 
 const storeRouter = express.Router();
 
-storeRouter.post("/", upload.single('avatar'), AddStore);// add retailer auth. after done with api.this adds the store to the mongoDB 
-storeRouter.post("/:id/products", upload.single('image'), addStoreProduct);// adds the product to the store as per the store_id
+storeRouter.post("/", authentication, upload.single('avatar'), AddStore);// add retailer auth. after done with api.this adds the store to the mongoDB 
+storeRouter.post("/:id/products", authentication, upload.single('image'), addStoreProduct);// adds the product to the store as per the store_id
 storeRouter.get("/", authentication, getStores);//returns all the stores in the mongoDb 
 storeRouter.get("/:id", authentication, getSpecificStore);//Returns the specific store with the store id
 storeRouter.get("/:id/products", authentication, getStoreProducts);//get products of the store who's id is passed from frontend
 storeRouter.get("/:storeId/products/:productId", authentication, getSpecificStoreProduct);//to get a specific product from a specific store
-storeRouter.delete("/:storeId", deleteStore);//deletes the whole store along with the products
-storeRouter.delete("/:storeId/products/:productId",deleteProduct);//deletes a specific product of a specific store
-storeRouter.put("/:storeId",upload.single('avatar'), updateStoreController);//updates the details of the store
-storeRouter.put("/:storeId/products/:productId", upload.single('image'), updateProductController);//updates a specific product within a specific store
+storeRouter.delete("/:storeId", authentication, deleteStore);//deletes the whole store along with the products
+storeRouter.delete("/:storeId/products/:productId", authentication, deleteProduct);//deletes a specific product of a specific store
+storeRouter.put("/:storeId", authentication, upload.single('avatar'), updateStoreController);//updates the details of the store
+storeRouter.put("/:storeId/products/:productId", authentication, upload.single('image'), updateProductController);//updates a specific product within a specific store
 
 export default storeRouter;
