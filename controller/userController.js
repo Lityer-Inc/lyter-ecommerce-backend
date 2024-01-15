a2903f3d7e29ad93ec5fd0d760a921f4c48baac4
Abdullahk70 <scientificgenius701924@gmail.com>
2024-01-15 21:04:36 +0500

    slight change

--- controller/userController.js
+++ controller/userController.js
@@ -235,9 +235,7 @@ export const getCartController = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    user.cart.map((itm)=>{
-      
-    })
+    res.status(200).json(user.cart);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
